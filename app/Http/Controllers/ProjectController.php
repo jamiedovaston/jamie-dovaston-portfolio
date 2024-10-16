72128d58c7aea21e482224e3a0013f8844e305b5
devdamo <minecraft.pop11@outlook.com>
2024-10-16 15:03:16 +0100

    added ProjectsController

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the projects on the welcome page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        // Fetch all projects from the database
+        $projects = Project::all();
+
+        // Return the welcome view and pass the projects
+        return view('welcome', compact('projects'));
+    }
+
+    /**
+     * Show the details of a specific project.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\View\View
+     */
+    public function show(Project $project)
+    {
+        // Return the project details view
+        return view('projects.show', compact('project'));
+    }
+}
