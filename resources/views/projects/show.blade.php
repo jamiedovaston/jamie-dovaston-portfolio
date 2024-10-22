da433b6f3bfab008d1fe04f5a7a1c8190df14207
Jamie Dovaston <jamiedovaston@outlook.com>
2024-10-22 19:26:12 +0100

    initial push

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -7,7 +7,7 @@
     <body class="overflow-y-scroll no-scrollbar" style="background-color: {{ $project->background_primary_color }};">
 
     <main class="pb-16 lg:pb-24 bg-zinc-900 antialiased min-h-screen" style="background-color: {{ $project->background_primary_color }};">
-        <div class="relative w-full h-[460px] xl:h-[460px]">
+        <div class="relative w-full h-[460px] xl:h-[400] xl:mt-5">
             <!-- Blurred Background -->
             <div class="absolute top-0 left-0 w-full h-full bg-[url('{{ $background_image }}')] bg-no-repeat bg-cover bg-center bg-blend-darken blur-3xl"></div>
 
@@ -26,7 +26,7 @@
                             @if(!empty($project->images) && is_array($project->images))
                                 <div id="animation-carousel" class="relative w-full" data-carousel="slide">
                                     <!-- Carousel wrapper with 16:9 aspect ratio -->
-                                    <div class="relative mb-4 overflow-hidden rounded-lg" style="padding-top: 56.25%;">
+                                    <div class="relative mb-2 overflow-hidden rounded-lg" style="padding-top: 56.25%;"> <!-- Reduced mb-4 to mb-2 -->
                                         <div class="absolute inset-0 image-gallery">
                                             @foreach($project->images as $index => $image)
                                                 <div class="{{ $index === 0 ? 'block' : 'hidden' }} duration-700 ease-in-out" data-carousel-item>
@@ -46,7 +46,9 @@
         </div>
 
         <!-- Flex container for article and sidebar -->
-        <div class="flex flex-col xl:flex-row relative z-20 justify-between p-6 mt-8 xl:mt-0 mx-4 max-w-screen-xl bg-zinc-800 xl:p-10 xl:mx-auto rounded-3xl" style="background-color: {{ $project->article_color }};">
+        <div class="flex flex-col xl:flex-row relative z-20 justify-between p-6 mt-0 xl:mt-0 mx-4 max-w-screen-xl bg-zinc-800 xl:p-9 xl:mx-auto rounded-3xl" style="background-color: {{ $project->article_color }};">
+            <!-- Reduced mt-8 to mt-4 -->
+
             <!-- Article Section -->
             <article class="xl:w-[828px] w-full max-w-none format format-sm sm:format-base lg:format-lg format-blue format-invert bg-black bg-opacity-15 rounded-2xl mb-6 xl:mb-0">
                 <div class="py-5 text-lg font-normal text-white p-5">{!! ($project->body) !!}</div>
